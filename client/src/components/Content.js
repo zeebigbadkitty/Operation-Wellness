4afda6f2c29d91fca6a3a610505d4767f9950040
Janvier Barreto Acevedo <janvieracevedo912gmail.com>
2023-03-02 21:25:48 -0600

    did some styling

--- client/src/components/Content.js
+++ client/src/components/Content.js
@@ -5,29 +5,33 @@ const columns = [
   { field: 'am', headerName: 'AM', width: 150 },
   { field: 'noon', headerName: 'NOON', width: 150 },
   { field: 'pm', headerName: 'PM', width: 160, },
+  { field: 'other', headerName: 'Other', width: 160, },
 ];
 
 const rows = [
-  { id: 1, noon: 'lexapro', am: 'Naloxen', pm:'Heloo' },
-  { id: 2, noon: 'Fetanyl', am: 'Rybelsus', pm: 'Cymbalta'},
-  { id: 3, noon: 'Oxycodon', am: 'Wegovy', pm:'Adderall'},
-  { id: 4, noon: 'Vicodin', am: 'Xanax', pm: 'Viagra'},
-  { id: 5, noon: 'Bunavail', am: 'Ativan', pm: 'Gilenya' },
-  { id: 6, noon: 'Nurtec', am: 'Acetominophen', pm: 'Brilinta' },
+  { id: 1, noon: 'lexapro', am: 'Naloxen', pm:'Heloo', other: 'Hello World' },
+  { id: 2, noon: 'Fetanyl', am: 'Rybelsus', pm: 'Cymbalta', other: 'Hello World'},
+  { id: 3, noon: 'Oxycodon', am: 'Wegovy', pm:'Adderall', other: 'Hello World'},
+  { id: 4, noon: 'Vicodin', am: 'Xanax', pm: 'Viagra', other: 'Hello World'},
+  { id: 5, noon: 'Bunavail', am: 'Ativan', pm: 'Gilenya', other: 'Hello World'},
+  { id: 6, noon: 'Nurtec', am: 'Acetominophen', pm: 'Brilinta', other: 'Hello World' },
+  { id: 6, noon: 'Nurtec', am: 'Acetominophen', pm: 'Brilinta', other: 'Hello World' },
+  { id: 6, noon: 'Nurtec', am: 'Acetominophen', pm: 'Brilinta', other: 'Hello World' },
+  { id: 6, noon: 'Nurtec', am: 'Acetominophen', pm: 'Brilinta', other: 'Hello World' },
+  { id: 6, noon: 'Nurtec', am: 'Acetominophen', pm: 'Brilinta', other: 'Hello World' },
 ];
 
 export default function DataTable() {
   return (
-    <div style={{ height: 400, width: '40%', }}>
+    <div style={{ height: 430, width: '45%', }}>
       <DataGrid
         rows={rows}
         columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
+        pageSize={10}
+        rowsPerPageOptions={[10]}
         checkboxSelection
-
-      
       />
     </div>
+    
   );
 }
