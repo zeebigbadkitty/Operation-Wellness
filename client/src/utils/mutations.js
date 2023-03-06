d0d38fd32c5bfeb0c023aae7bb19ddc1b34a82c4
Cesar Castillo <castillocesar2412@gmail.com>
2023-03-05 20:37:06 -0600

    added login and log out mutations to the front and backend of mutations.js file

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -17,3 +17,29 @@ export const ADD_SAVED_DRUGS = gql`
     }
 `;
 
+export const LOGIN_MUTATION = gql`
+  mutation Login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        name
+        email
+        savedDrugs {
+          _id
+          productndc
+          producttypename
+          active_numerator_strength
+          active_ingred_unit
+        }
+      }
+    }
+  }
+`;
+
+export const LOGOUT_MUTATION = gql`
+  mutation {
+    logout
+  }
+`;
+
