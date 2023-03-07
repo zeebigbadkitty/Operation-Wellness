703bf796095584a0fea3e2a354bdb63922184ca1
Socially Awkward Streams <candice.radam@gmail.com>
2023-03-06 19:47:33 -0600

    Added the token to the app.js.

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import Login from './components/Login';
 import Signup from './components/Signup';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { setContext } from '@apollo/client/link/context';
 
 // const { gql } = require("apollo-server-express");
 
@@ -49,6 +50,19 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 const client = new ApolloClient({
   uri: '/graphql',
   cache: new InMemoryCache(),
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
 });
 
 function Copyright() {
