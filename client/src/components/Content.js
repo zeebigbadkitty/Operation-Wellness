be917172541bf2ea5a6e66bed28bdae1685755b9
Socially Awkward Streams <candice.radam@gmail.com>
2023-03-06 22:16:20 -0600

    Completed user data seed, and queries onto the content.js.

--- client/src/components/Content.js
+++ client/src/components/Content.js
@@ -1,63 +1,39 @@
-import * as React from 'react';
-import { DataGrid } from '@mui/x-data-grid';
-import axios from "axios"
+import * as React from "react";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import axios from "axios";
+import { useQuery } from "@apollo/client";
+import { QUERY_USER_DRUGS, QUERY_SINGLE_DRUG } from "../utils/queries";
 
+//use query hook from Apollo to get data from the server
 
 const columns = [
-  { field: 'company', headerName: 'Company', width: 150 },
-  { field: 'boosters', headerName: 'Boosters', width: 150 },
-  { field: 'rocket_name', headerName: 'Rocket Name', width: 160, },
-  { field: 'first_flight', headerName: 'First Flight', width: 160, },
-  { field: 'country', headerName: 'Country', width: 160, },
+  { field: "proprietaryname", headerName: "Drug Name", width: 150 },
+  { field: "active_numerator_strength", headerName: "Strength", width: 150 },
+  { field: "active_ingred_unit", headerName: "Strength Unit", width: 160 },
 ];
 
-
-
-export default function DataTable() { 
+export default function DataTable() {
   const [rows, setRows] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
 
-  React.useEffect(() => {
-    const getData = async () => {
-      try {
-
-        const response = await fetch('/drugs');
-        const data = await response.json();
-        setRows(data);
-        setIsLoading(false);
-        
-        
-      } catch (error) {
-        console.error(error);
-        setIsLoading(false);
-      }
-    };
-    getData();
-  }, []);
-
-  return isLoading ? null : (
-    <div style={{ height: 430, width: '60%', }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        pageSize={10}
-        rowsPerPageOptions={[10]}
-        checkboxSelection
-      />
-    </div>
+  const { loading, data } = useQuery(QUERY_USER_DRUGS);
+  const userDrugs = data?.userDrugs || [];
+  console.log(userDrugs)
+  return (
+    <>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div style={{ height: 430, width: "60%" }}>
+          <DataGrid
+            columns={columns}
+            rows={userDrugs}
+            slots={{
+              toolbar: GridToolbar,
+            }}
+          />
+        </div>
+      )}
+    </>
   );
 }
-
-
-const temp = [
-  { id: 1, noon: 'lexapro', am: 'Naloxen', pm:'Heloo', other: 'Hello World' },
-  { id: 2, noon: 'Fetanyl', am: 'Rybelsus', pm: 'Cymbalta', other: 'Hello World'},
-  { id: 3, noon: 'Oxycodon', am: 'Wegovy', pm:'Adderall', other: 'Hello World'},
-  { id: 4, noon: 'Vicodin', am: 'Xanax', pm: 'Viagra', other: 'Hello World'},
-  { id: 5, noon: 'Bunavail', am: 'Ativan', pm: 'Gilenya', other: 'Hello World'},
-  { id: 6, noon: 'Nurtec', am: 'Acetominophen', pm: 'Brilinta', other: 'Hello World' },
-  { id: 6, noon: 'Nurtec', am: 'Acetominophen', pm: 'Brilinta', other: 'Hello World' },
-  { id: 6, noon: 'Nurtec', am: 'Acetominophen', pm: 'Brilinta', other: 'Hello World' },
-  { id: 6, noon: 'Nurtec', am: 'Acetominophen', pm: 'Brilinta', other: 'Hello World' },
-  { id: 6, noon: 'Nurtec', am: 'Acetominophen', pm: 'Brilinta', other: 'Hello World' },
-];
--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,12 +1,12 @@
 const db = require('../config/connection');
-const { Drugs } = require('../models');
+const { Drug } = require('../models');
 const drugSeeds = require('./drugs2.json');
 
 db.once('open', async () => {
   console.log('hello')
   try {
-    await Drugs.collection.drop(); //if we're using multiple files, remove this.
-    await Drugs.create(drugSeeds);
+    await Drug.collection.drop(); //if we're using multiple files, remove this.
+    await Drug.create(drugSeeds);
 
     console.log('all done!');
    
