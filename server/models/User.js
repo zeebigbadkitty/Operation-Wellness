2c72667e81d8ecf60ca4b8ecd9d4c8f0812e6e28
Socially Awkward Streams <candice.radam@gmail.com>
2023-03-07 18:10:30 -0600

    Another push before class.

--- server/models/User.js
+++ server/models/User.js
@@ -32,9 +32,19 @@ const userSchema = new Schema({
   
 });
 
-userSchema.methods.isCorrectPassword = async function(password) {
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// custom method to compare and validate password for logging in
+userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
-}
+};
 
 const User = mongoose.model('User', userSchema);
 
